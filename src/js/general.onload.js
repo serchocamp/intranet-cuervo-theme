f25ec4b1f47625f1fbad158e72b31f2582b5d39b
serchocamp <socampo@consistent.com.mx>
2019-10-03 09:16:18 -0500

    cambios file general.onload

--- src/js/general.onload.js
+++ src/js/general.onload.js
@@ -1,27 +1,27 @@
 /* Inicializacion del carrusel */
 $(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:25,
-        dots:false,
-        navText: [ '<i class="fas fa-chevron-left"></i>' , '<i class="fas fa-chevron-right"></i>' ],
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
-            },
-            600:{
-                items:3,
-                nav:false
-            },
-            1000:{
-                items:5,
-                nav:true,
-                loop:false
-            }
-        }
-    })
+  	$('.owl-carousel').owlCarousel({
+  	    loop:true,
+  	    margin:25,
+  	    dots:false,
+  	    navText: [ '<i class="fas fa-chevron-left"></i>' , '<i class="fas fa-chevron-right"></i>' ],
+  	    responsiveClass:true,
+  	    responsive:{
+  	        0:{
+  	            items:1,
+  	            nav:true
+  	        },
+  	        600:{
+  	            items:3,
+  	            nav:false
+  	        },
+  	        1000:{
+  	            items:5,
+  	            nav:true,
+  	            loop:false
+  	        }
+  	    }
+  	})
 });
 
 /* Inicializacion de popvers */
@@ -63,6 +63,13 @@ $(document).ready(function(){
     })
 });
 
+/* Oculta el menu hasta que se abra el dropdown */
+$(document).ready(function(){
+  $('#collapseMarcas').on('shown.bs.collapse', function () {
+    $('.owl-carousel-menu').css('opacity' , '1');
+  });
+});
+
 /* SOMOS JOSE CUERVO */
 /* Funcion para activar los tabs por medio de un select */
 $(document).ready(function(){
@@ -88,4 +95,63 @@ $(document).ready(function(){
       $(this).parent().slideUp();
     })
     
+});
+
+
+/* Galeria de productos (Galleriffic) */
+jQuery(document).ready(function(e) {
+  // We only want these styles applied when javascript is enabled
+  $('div.navigation').css({'width' : '300px', 'float' : 'left'});
+  $('div.content').css('display', 'block');
+
+  $(".each-gallery").each(function(i){
+    // Initialize Advanced Galleriffic Gallery
+    var gallery = $('#thumbs'+i).galleriffic({
+      delay:                     2500,
+      numThumbs:                 15,
+      preloadAhead:              10,
+      enableTopPager:            true,
+      enableBottomPager:         true,
+      maxPagesToShow:            7,
+      imageContainerSel:         '#slideshow'+ i,
+      controlsContainerSel:      '#controls' + i,
+      captionContainerSel:       '#caption' + i,
+      loadingContainerSel:       '#loading' + i,
+      renderSSControls:          true,
+      renderNavControls:         true,
+      playLinkText:              'Play Slideshow',
+      pauseLinkText:             'Pause Slideshow',
+      prevLinkText:              '&lsaquo; Previous Photo',
+      nextLinkText:              'Next Photo &rsaquo;',
+      nextPageLinkText:          'Next &rsaquo;',
+      prevPageLinkText:          '&lsaquo; Prev',
+      enableHistory:             false,
+      autoStart:                 false,
+      syncTransitions:           true,
+      defaultTransitionDuration: 900,
+      onSlideChange:             function(prevIndex, nextIndex) {
+        // 'this' refers to the gallery, which is an extension of $('#thumbs')
+        this.find('ul.thumbs').children()
+          .eq(prevIndex).fadeTo('fast', onMouseOutOpacity).end()
+          .eq(nextIndex).fadeTo('fast', 1.0);
+      },
+      onPageTransitionOut:       function(callback) {
+        this.fadeTo('fast', 0.0, callback);
+      },
+      onPageTransitionIn:        function() {
+        this.fadeTo('fast', 1.0);
+      }
+    });
+  });
+
+  // Initially set opacity on thumbs and add
+  // additional styling for hover effect on thumbs
+  var onMouseOutOpacity = 0.67;
+  $('ul.thumbs li').opacityrollover({
+    mouseOutOpacity:   onMouseOutOpacity,
+    mouseOverOpacity:  1.0,
+    fadeSpeed:         'fast',
+    exemptionSelector: '.selected'
+  });
+  
 });
